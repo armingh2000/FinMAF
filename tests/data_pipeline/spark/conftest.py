137f1f468c50fd1f01dce6144f13170d922c19e0
armingh2000 <armingh379@gmail.com>
2023-11-18 18:54:52 -0500

    a dps data processing tests, a conftest.py

--- tests/data_pipeline/spark/conftest.py
+++ tests/data_pipeline/spark/conftest.py
@@ -2,6 +2,11 @@ import pytest
 import os
 import csv
 from pyspark.sql import SparkSession
+from pathlib import Path
+import src.configs as configs
+import pytest
+from unittest.mock import Mock
+import shutil
 
 
 @pytest.fixture(scope="session")
@@ -11,17 +16,83 @@ def spark_session():
     spark.stop()
 
 
-# @pytest.fixture(scope="session")
-# def create_csv_files(tmp_path):
-#     # Define CSV files and their content
-#     files_content = {
-#         "test_data1.csv": [("file_name", "column_with_null"), ("row1_col1", "row1_col2")],
-#         "test_data2.csv": [("headerA", "headerB"), ("rowA_colA", "rowA_colB")],
-#     }
+@pytest.fixture
+def clean_tmp_path(tmp_path):
+    # Cleanup code to manually clear the tmp_path
+    for item in tmp_path.iterdir():
+        if item.is_dir():
+            shutil.rmtree(item)
+        else:
+            item.unlink()
 
-#     # Create CSV files
-#     for file_name, content in files_content.items():
-#         file_path = os.path.join(tmp_path, file_name)
-#         with open(file_path, "w", newline="") as file:
-#             writer = csv.writer(file)
-#             writer.writerows(content)
+
+@pytest.fixture
+def create_csv_files(tmp_path, clean_tmp_path, mock_configs, mkdirs):
+    # Define CSV files and their content
+    files_content = {
+        "test_data1.csv": [
+            [
+                "Date",
+                "Open",
+                "High",
+                "Low",
+                "Close",
+                "Adj Close",
+                "Volume",
+                "Repaired?",
+            ],
+            ["2022-01-01", 1.0, 2.0, 3.0, 4.0, 5.0, 6, False],
+            [None, 7.0, 8.0, 9.0, 10.0, 11.0, 12, True],
+            ["2022-01-03", None, 14.0, 15.0, 16.0, 17.0, 18, False],
+            ["2022-01-04", 19.0, None, 21.0, 22.0, 23.0, 24, True],
+            ["2022-01-05", 25.0, 26.0, None, 28.0, 29.0, 30, False],
+            ["2022-01-06", 31.0, 32.0, 33.0, None, 35.0, 36, True],
+            ["2022-01-06", 31.0, 32.0, 33.0, None, 35.0, 36, True],  # DUPLICATE
+        ],
+        "test_data2.csv": [
+            [
+                "Date",
+                "Open",
+                "High",
+                "Low",
+                "Close",
+                "Adj Close",
+                "Volume",
+                "Repaired?",
+            ],
+            ["2022-01-01", 1.0, 2.0, 3.0, 4.0, 5.0, 6, False],
+            ["2022-01-02", 37.0, 38.0, 39.0, 40.0, None, 42, False],
+            ["2022-01-03", 43.0, 44.0, 45.0, 46.0, 47.0, None, True],
+            ["2022-01-04", 48.0, 49.0, 50.0, 51.0, 52.0, 53, None],
+            ["2022-01-04", 48.0, 49.0, 50.0, 51.0, 52.0, 53, None],  # DUPLICATE
+        ],
+    }
+
+    # Create CSV files
+    for file_name, content in files_content.items():
+        file_path = os.path.join(configs.dps_raw, file_name)
+        with open(file_path, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(content)
+
+
+@pytest.fixture
+def mkdirs(mock_configs):
+    configs.dps_raw.mkdir(exist_ok=True)
+    configs.dps_clean.mkdir(exist_ok=True)
+
+
+@pytest.fixture
+def mock_configs(monkeypatch, tmp_path):
+    # Use monkeypatch to replace yf.download with the mock function
+    dps_raw = Path(tmp_path / "raw/")
+    dps_raw.mkdir(exist_ok=True)
+    dps_clean = Path(tmp_path / "clean/")
+    dps_clean.mkdir(exist_ok=True)
+    monkeypatch.setattr(configs, "dps_raw", dps_raw)
+    monkeypatch.setattr(configs, "dps_clean", dps_clean)
+
+
+@pytest.fixture
+def mock_logger():
+    return Mock()
--- tests/data_pipeline/spark/test_data_processing.py
+++ tests/data_pipeline/spark/test_data_processing.py
@@ -3,27 +3,19 @@ from src.data_pipeline.spark.data_processing import (
     clean_stock_data,
     process,
 )
-from unittest.mock import Mock
 import pandas as pd
-from pathlib import Path
-import pytest
 import src.configs as configs
-
-
-@pytest.fixture(scope="module")
-def mock_logger():
-    return Mock()
-
-
-@pytest.fixture
-def mock_configs(monkeypatch, tmp_path):
-    # Use monkeypatch to replace yf.download with the mock function
-    dps_raw = Path(tmp_path / "raw/")
-    dps_raw.mkdir()
-    dps_clean = Path(tmp_path / "clean/")
-    dps_clean.mkdir()
-    monkeypatch.setattr(configs, "dps_raw", dps_raw)
-    monkeypatch.setattr(configs, "dps_clean", dps_clean)
+import os
+import shutil
+from pyspark.sql.functions import col
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    DateType,
+    DoubleType,
+    LongType,
+    BooleanType,
+)
 
 
 def test_dump_nulls(mock_logger, mock_configs, spark_session):
@@ -35,15 +27,15 @@ def test_dump_nulls(mock_logger, mock_configs, spark_session):
     # Date,Open,High,Low,Close,Adj Close,Volume,Repaired?
     test_df = spark_session.createDataFrame(
         [
-            ["2022-01-01", 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, False],
-            [None, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, True],
-            ["2022-01-03", None, 14.0, 15.0, 16.0, 17.0, 18.0, False],
-            ["2022-01-04", 19.0, None, 21.0, 22.0, 23.0, 24.0, True],
-            ["2022-01-05", 25.0, 26.0, None, 28.0, 29.0, 30.0, False],
-            ["2022-01-06", 31.0, 32.0, 33.0, None, 35.0, 36.0, True],
-            ["2022-01-07", 37.0, 38.0, 39.0, 40.0, None, 42.0, False],
+            ["2022-01-01", 1.0, 2.0, 3.0, 4.0, 5.0, 6, False],
+            [None, 7.0, 8.0, 9.0, 10.0, 11.0, 12, True],
+            ["2022-01-03", None, 14.0, 15.0, 16.0, 17.0, 18, False],
+            ["2022-01-04", 19.0, None, 21.0, 22.0, 23.0, 24, True],
+            ["2022-01-05", 25.0, 26.0, None, 28.0, 29.0, 30, False],
+            ["2022-01-06", 31.0, 32.0, 33.0, None, 35.0, 36, True],
+            ["2022-01-07", 37.0, 38.0, 39.0, 40.0, None, 42, False],
             ["2022-01-08", 43.0, 44.0, 45.0, 46.0, 47.0, None, True],
-            ["2022-01-09", 48.0, 49.0, 50.0, 51.0, 52.0, 53.0, None],
+            ["2022-01-09", 48.0, 49.0, 50.0, 51.0, 52.0, 53, None],
         ],
         schema=[
             "Date",
@@ -89,3 +81,71 @@ def test_dump_nulls(mock_logger, mock_configs, spark_session):
     expected_df = pd.DataFrame(expected_data)
 
     assert nulls_df.equals(expected_df)
+
+
+def test_clean_stock_data(mock_logger, mock_configs, spark_session, create_csv_files):
+    clean_stock_data(spark_session, mock_logger)
+
+    # Check if csv clean files are created
+    assert set(["test_data1.csv", "test_data2.csv"]).issubset(
+        os.listdir(configs.dps_clean)
+    )
+
+    # Check for nulls.csv creation
+    assert os.path.exists(configs.dps_clean / "nulls.csv")
+
+    # Define the schema
+    schema = StructType(
+        [
+            StructField("Date", DateType(), True),
+            StructField("Open", DoubleType(), True),
+            StructField("High", DoubleType(), True),
+            StructField("Low", DoubleType(), True),
+            StructField("Close", DoubleType(), True),
+            StructField("Adj_close", DoubleType(), True),
+            StructField("Volume", LongType(), True),
+            StructField("Repaired?", BooleanType(), True),
+        ]
+    )
+
+    # Check for removal of duplicates
+    test_df1_path = os.path.join(configs.dps_clean, "test_data1.csv")
+    test_df2_path = os.path.join(configs.dps_clean, "test_data2.csv")
+    test_df1 = spark_session.read.csv(test_df1_path, header=True, schema=schema)
+    test_df2 = spark_session.read.csv(test_df2_path, header=True, schema=schema)
+
+    duplicate_dates1 = test_df1.groupBy("date").count().filter(col("count") > 1)
+    duplicate_dates2 = test_df2.groupBy("date").count().filter(col("count") > 1)
+
+    assert duplicate_dates1.count() == 0
+    assert duplicate_dates2.count() == 0
+
+    # Check for data types
+    assert test_df1.dtypes == [
+        ("Date", "date"),
+        ("Open", "double"),
+        ("High", "double"),
+        ("Low", "double"),
+        ("Close", "double"),
+        ("Adj_close", "double"),
+        ("Volume", "bigint"),
+        ("Repaired?", "boolean"),
+    ]
+
+    assert test_df2.dtypes == [
+        ("Date", "date"),
+        ("Open", "double"),
+        ("High", "double"),
+        ("Low", "double"),
+        ("Close", "double"),
+        ("Adj_close", "double"),
+        ("Volume", "bigint"),
+        ("Repaired?", "boolean"),
+    ]
+
+    # Check for contents of nulls.csv
+    nulls_df = pd.read_csv(configs.dps_clean / "nulls.csv")
+
+    assert set(["test_data1.csv", "test_data2.csv"]).issubset(
+        list(nulls_df["file_name"])
+    )
