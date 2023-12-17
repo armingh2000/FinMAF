1fb619463b0f5126d26af334090cb79032d0979b
armingh2000 <armingh379@gmail.com>
2023-12-17 13:45:51 -0500

    update configs
    add log configs for embeddings, add pca components config

--- src/configs/log.py
+++ src/configs/log.py
@@ -1,7 +1,11 @@
 from .shared import project_root
 
-# Logging configurations
+# Data Pipeline Logging Configurations
 dps_download_log_name = "dps_download"
 dps_download_log_path = project_root / "logs/dps_download.log"
 dps_process_log_name = "dps_process"
 dps_process_log_path = project_root / "logs/dps_process.log"
+
+# Model Train Logging Configurations
+embedding_log_name = "mt_embedding"
+embedding_log_path = project_root / "logs/mt_embedding.log"
