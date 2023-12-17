d13a3b7f7cece292f27b7972941d0a2dd2efceec
armingh2000 <armingh379@gmail.com>
2023-12-17 12:20:16 -0500

    add embedding.py, utils.py
    add stock embedding generator and model to embedding.py, add h5py dumping to uril.py

---
+++ src/model/train/embedding.py
@@ -0,0 +1,59 @@
+from transformers import BertModel, BertTokenizer
+import torch
+from tqdm import tqdm
+from sklearn.decomposition import PCA
+
+
+def get_embedding_input(metadata):
+    inputs = {}
+
+    for _, row in tqdm(metadata.iterrows()):
+        if row["Symbol"] == "":
+            inputs["NA"] = row["Security Name"]
+        else:
+            inputs[row["Symbol"]] = row["Security Name"]
+
+        # region This is the original code
+        # inputs[row["Symbol"]] = row["Security Name"]
+        # endregion
+
+    return inputs
+
+
+def reduce_dimensions(embeddings, n_components=50):
+    pca = PCA(n_components=n_components)
+    reduced_embeddings = pca.fit_transform(embeddings)
+    return reduced_embeddings
+
+
+class EmbeddingModel:
+    def __init__(self, model_name="ProsusAI/finbert"):
+        self.model, self.tokenizer = self.load_embedding_model(model_name)
+
+    def load_embedding_model(self, model_name):
+        # Load pre-trained model and tokenizer
+        model = BertModel.from_pretrained(model_name)
+        tokenizer = BertTokenizer.from_pretrained(model_name)
+        return model, tokenizer
+
+    def __call__(self, symbol, description):
+        with torch.no_grad():
+            # Tokenize the stock symbols with their descriptions and get embeddings
+            # The tokenizer automatically adds [CLS] and [SEP] tokens
+            inputs = self.tokenizer(
+                symbol, description, return_tensors="pt", padding=True, truncation=True
+            )
+            output = self.model(**inputs)
+            cls_hidden_state = output.last_hidden_state[:, 0, :]
+
+            return cls_hidden_state
+
+
+def get_embeddings(embedding_inputs):
+    embeddings = {}
+    embedding_model = EmbeddingModel()
+
+    for symbol, description in tqdm(embedding_inputs.items()):
+        embeddings[symbol] = embedding_model(symbol, description)
+
+    return embeddings
