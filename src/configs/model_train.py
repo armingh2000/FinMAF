f2d68d5bba1e2c95aa17a8338fd2fa678642901e
armingh2000 <armingh379@gmail.com>
2023-12-17 11:17:39 -0500

    update configs
    update shared.py, add model_train.py configs

---
+++ src/configs/model_train.py
@@ -0,0 +1,7 @@
+from .shared import project_root
+import torch
+
+
+bert_embedding_path = project_root / "data/historical/train/bert_embeddings.h5"
+pca_embedding_path = project_root / "data/historical/train/psa_embeddings.h5"
+use_cuda = torch.cuda.is_available()
--- src/configs/shared.py
+++ src/configs/shared.py
@@ -7,4 +7,4 @@ current_file_path = Path(__file__).resolve()
 project_root = current_file_path.parent.parent.parent  # Adjust with .parent as needed
 dps_raw = project_root / "data/historical/raw/"  # raw historical data path
 dps_clean = project_root / "data/historical/clean/"  # clean historical data path
-stocks_metadata = dps_raw / "meta_data/symbols_valid_meta.csv"
+valid_stocks_metadata = dps_raw / "meta_data/symbols_valid_meta.csv"
