22dea3ce39c60ff76b32c9e3b00542d34f2cc5cd
armingh2000 <armingh379@gmail.com>
2023-12-17 11:12:48 -0500

    remove unneccesary import

--- src/model/train/data.py
+++ src/model/train/data.py
@@ -1,27 +1,12 @@
-from pyspark.sql.types import (
-    StructType,
-    StructField,
-    DateType,
-    DoubleType,
-    LongType,
-    BooleanType,
-)
 import pandas as pd
-from tqdm import tqdm
-import os
 import src.configs as configs
 
 
 def get_stock_metadata():
-    metadata = pd.read_csv(configs.stocks_metadata)
+    metadata = pd.read_csv(
+        configs.valid_stocks_metadata,
+        dtype={"Symbol": str, "NASDAQ Symbol": str},
+        keep_default_na=False,  # This prevents Pandas from interpreting "NA" as NaN
+    )
 
     return metadata
-
-
-def get_embedding_input(metadata):
-    inputs = []
-
-    for _, row in tqdm(metadata.iterrows()):
-        inputs.append([row["NASDAQ Symbol"], row["Security Name"]])
-
-    return inputs
