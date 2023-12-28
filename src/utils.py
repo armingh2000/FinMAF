34c1bef4a67f3e4293be1f2d3d394aae66365bdb
armingh2000 <armingh379@gmail.com>
2023-12-28 11:06:53 -0500

    generalize dump_dictionary
    change dumping to accept any kind (that is already used not all) of
    dictionaries, moved to shared utils (src.utils)

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 import os
+import h5py
+import numpy as np
+import torch
 
 
 def mkpath(path):
@@ -22,3 +25,50 @@ def mkpath(path):
         print(f"Created path: {path}")
     else:
         print(f"Path already exists: {path}")
+
+
+def dump_dictionary(dictionary, filepath):
+    mkpath(filepath)
+    with h5py.File(filepath, "w") as file:
+        for key, value in dictionary.items():
+            # Check if value is a numpy array
+            if isinstance(value, np.ndarray):
+                ds = file.create_dataset(key, data=value)
+                ds.attrs["type"] = "numpy_array" if value.shape else "numpy_scalar"
+            # Check if value is a torch.Tensor
+            elif isinstance(value, torch.Tensor):
+                tensor_np = value.numpy()  # Convert tensor to numpy array
+                ds = file.create_dataset(key, data=tensor_np)
+                ds.attrs["type"] = "torch_tensor"
+            # Check if value is an integer
+            elif isinstance(value, int):
+                ds = file.create_dataset(key, (), dtype="i")  # Create a scalar dataset
+                ds[()] = value  # Assign the integer value
+                ds.attrs["type"] = "int"
+            else:
+                raise TypeError(f"Unsupported type for key '{key}': {type(value)}")
+
+
+def load_dictionary(filepath):
+    if not os.path.exists(filepath):
+        raise ValueError(f"Filepath {filepath} does not exist")
+
+    with h5py.File(filepath, "r") as file:
+        dictionary = {}
+        for key in file:
+            item = file[key]
+            item_type = item.attrs["type"]
+            if item_type == "numpy_array":
+                dictionary[key] = item[:]
+            elif item_type == "numpy_scalar":
+                dictionary[key] = np.array(item[()])
+            elif item_type == "torch_tensor":
+                dictionary[key] = torch.from_numpy(
+                    item[:]
+                )  # Convert numpy array back to torch.Tensor
+            elif item_type == "int":
+                dictionary[key] = item[()]
+            else:
+                raise ValueError(f"Unknown type for key '{key}': {item_type}")
+
+        return dictionary
