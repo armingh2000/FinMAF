96021fe73bae5b67516e514db616df88e60c224e
armin <armingh379@gmail.com>
2025-01-03 12:05:36 -0500

    update pythonpath to run in streamlit community cloud

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+
+import os
+import sys
+
+# Get the current directory
+current_dir = os.getcwd()
+sys.path.insert(0, current_dir)
+
 from src.configs import input_form_page, analyze_page
 
 pg = st.navigation([input_form_page, analyze_page])
